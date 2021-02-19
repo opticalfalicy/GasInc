39cc76533b0c3b629db63d6b0022b0cfb0126156
b <opticalfalicy@gmail.com>
2021-02-18 20:29:50 -0600

    commented out public dir

--- gsibackend/settings.py
+++ gsibackend/settings.py
@@ -89,7 +89,7 @@ TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
         'DIRS': [
-        	os.path.join(BASE_DIR, 'public'),
+        	# os.path.join(BASE_DIR, 'public'),
         ],
         'APP_DIRS': True,
         'OPTIONS': {
