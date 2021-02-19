4fc78b79b291423d9886e024cb4450964172e488
b <opticalfalicy@gmail.com>
2021-02-18 19:54:45 -0600

    allowed host

--- gsibackend/settings.py
+++ gsibackend/settings.py
@@ -34,7 +34,7 @@ DEBUG = os.getenv("DEBUG", "False") == "True"
 
 DEVELOPMENT_MODE = os.getenv("DEVELOPMENT_MODE", "False") == "True"
 
-ALLOWED_HOSTS = os.getenv("DJANGO_ALLOWED_HOSTS", "127.0.0.1,localhost").split(",")
+ALLOWED_HOSTS = os.getenv("DJANGO_ALLOWED_HOSTS", "127.0.0.1,localhost,gas-inc-9jjmh.ondigitalocean.app").split(",")
 
 CORS_ORIGIN_WHITELIST = [
     'http://localhost:3000',
