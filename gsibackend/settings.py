6a2d5906656652366af9ff36394f5948791ef970
b <opticalfalicy@gmail.com>
2021-03-24 20:52:36 -0500

    moved files to backend folder

--- gsibackend/settings.py
+++ gsibackend/settings.py
@@ -27,9 +27,6 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
-
-ALLOWED_HOSTS = ['127.0.0.1', 'localhost', '159.203.37.196']
 
 CORS_ORIGIN_WHITELIST = [
     'http://localhost:3000',
@@ -93,13 +90,6 @@ WSGI_APPLICATION = 'gsibackend.wsgi.application'
 
 # Database
 # https://docs.djangoproject.com/en/3.1/ref/settings/#databases
-
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
-}
 
 
 # Password validation
